60b1bc65ddedc3d9e5dfde602ef359812f32792d
Diwakart09 <39313486+Diwakart09@users.noreply.github.com>
2020-10-15 15:15:52 +0530

    Removed unused lines

--- draw_geo.py
+++ draw_geo.py
@@ -2,15 +2,13 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import RegularPolygon
 
 
-offCoord = [[-2,-2],[-1,-2],[0,-2],[1,-2],[2,-2]]
-
-offCoord = [[4,4]]
+Coord = [[4,4]]
 
 fig, ax = plt.subplots(1)
 ax.set_aspect('equal')
 
-for c in offCoord:
-    hex = RegularPolygon((c[0], c[1]), numVertices=4, radius=2./3., alpha=0.2, edgecolor='b')
+for c in Coord:
+    hex = RegularPolygon((c[0], c[1]), numVertices=6, radius=2./3., alpha=0.2, edgecolor='b')
     ax.add_patch(hex)
 
 circle = plt.Circle((4, 4), radius=0.5, fc='r')
